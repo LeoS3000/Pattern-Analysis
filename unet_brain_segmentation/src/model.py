091619e0833769f697a5f9cef5719d6a9222b257
LEO STRIETZEL <leostrietzel@gmail.com>
2025-10-10 12:27:22 +1000

    Refactor UNet to support 3D operations: update architecture and add 3D building blocks

--- unet_brain_segmentation/src/model.py
+++ unet_brain_segmentation/src/model.py
@@ -60,22 +60,22 @@ class OutConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-class UNet(nn.Module):
+class UNet3D(nn.Module):
     def __init__(self, n_channels, n_classes):
-        super(UNet, self).__init__()
+        super(UNet3D, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
 
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
-        self.down4 = Down(512, 1024)
-        self.up1 = Up(1024, 512)
-        self.up2 = Up(512, 256)
-        self.up3 = Up(256, 128)
-        self.up4 = Up(128, 64)
-        self.outc = OutConv(64, n_classes)
+        self.inc = DoubleConv3D(n_channels, 64)
+        self.down1 = Down3D(64, 128)
+        self.down2 = Down3D(128, 256)
+        self.down3 = Down3D(256, 512)
+        self.down4 = Down3D(512, 1024)
+        self.up1 = Up3D(1024, 512)
+        self.up2 = Up3D(512, 256)
+        self.up3 = Up3D(256, 128)
+        self.up4 = Up3D(128, 64)
+        self.outc = OutConv3D(64, n_classes)
 
     def forward(self, x):
         x1 = self.inc(x)
@@ -89,3 +89,61 @@ class UNet(nn.Module):
         x = self.up4(x, x1)
         logits = self.outc(x)
         return logits
+
+# 3D versions of the building blocks
+
+class DoubleConv3D(nn.Module):
+    """(convolution => [BN] => ReLU) * 2 for 3D"""
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv3d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm3d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+class Down3D(nn.Module):
+    """Downscaling with maxpool then double conv for 3D"""
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool3d(2),
+            DoubleConv3D(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+class Up3D(nn.Module):
+    """Upscaling then double conv for 3D"""
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up = nn.ConvTranspose3d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+        self.conv = DoubleConv3D(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffD = x2.size()[2] - x1.size()[2]
+        diffH = x2.size()[3] - x1.size()[3]
+        diffW = x2.size()[4] - x1.size()[4]
+        x1 = F.pad(x1, [diffW // 2, diffW - diffW // 2,
+                        diffH // 2, diffH - diffH // 2,
+                        diffD // 2, diffD - diffD // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+class OutConv3D(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv3D, self).__init__()
+        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
